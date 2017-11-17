481cbe4abd4b4206d1a05f6a49d15ba087a277c8
Patrick <patricksmccardle@gmail.com>
2017-11-17 15:36:35 -0500

    adding something.sh

---
+++ testAutomation/testCases/something.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+variable="undefined"
+path_to_test_file="/home/patrick/KMP/testAutomation/project/enketo/enketo-express-2/test/server/testCases/testCase1.spec.js"
+
+cat > $path_to_test_file <<EOF
+/* global describe, require, it, afterEach */
+'use strict';
+
+// safer to ensure this here (in addition to grunt:env:test)
+process.env.NODE_ENV = 'test';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var chaiAsPromised = require( 'chai-as-promised' );
+var redis = require( 'redis' );
+var config = require( '../../app/models/config-model' ).server;
+config[ 'base path' ] = '';
+var submission = require( '../../app/models/submission-model' );
+var client = redis.createClient( config.redis.main.port, config.redis.main.host, {
+    auth_pass: config.redis.main.password
+} );
+
+chai.use( chaiAsPromised );
+
+describe( 'Survey Model', function() {
+
+  afterEach( function( done ) {
+    // select test database and flush it
+    client.select( 15, function( err ) {
+      if ( err ) {
+        return done( err );
+      }
+      client.flushdb( function( err ) {
+        if ( err ) {
+          return done( err );
+        }
+        done();
+      });
+    });
+  });
+
+  describe( 'isNew() check', function() {
+    var id = 'AAAA';
+    var instanceId = 'uuid:BBBB';
+
+    it( 'Test: 001 is rejected if id is undefined', function() {
+      var test = submission.isNew( $variable, instanceId );
+      return expect( test ).to.eventually.be.rejected.and.have.property( 'status' ).that.equals( 400 );
+    });
+  })
+})
+EOF
