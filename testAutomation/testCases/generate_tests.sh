d26908e24aca6cad75ab45aa7d88fccbd58a0e38
Patrick <patricksmccardle@gmail.com>
2017-11-28 19:05:52 -0500

    adding all test cases

--- testAutomation/testCases/generate_tests.sh
+++ testAutomation/testCases/generate_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -ex
+ set -x
 
 . something.sh
 
@@ -16,14 +16,48 @@ PATH_TO_ENKETO_DOCKER_COMPOSE="$PATH_TO_ENKETO/docker-compose.yml"
 
 PATH_TO_KMP_ID_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/id_test_results"
 
+PATH_TO_KMP_INSTANCEID_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/instanceId_test_results"
+
+PATH_TO_KMP_GET_REQUEST_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/get_request_test_results"
+
+PATH_TO_KMP_SET_SURVEY_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/set_survey_test_results"
+
+PATH_TO_KMP_CACHE_INSTANCE_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/cache_instance_test_results"
+
+PATH_TO_KMP_OBTAIN_CACHED_INSTANCE_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/obtain_cached_instance_test_results"
+
+PATH_TO_ALL_TEST_RESULTS="/home/patrick/KMP/testAutomation/reports/all_test_results"
+
 run_id_tests () {
   mkdir -p "$PATH_TO_KMP_ID_TEST_RESULTS"
   #docker-compose -f "$PATH_TO_ENKETO_DOCKER_COMPOSE" exec enketo sh -c "grunt test-and-build-kmp_id"
   docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_id'
 }
+run_instanceId_tests () {
+ mkdir -p "$PATH_TO_KMP_INSTANCEID_TEST_RESULTS"
+ docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_instanceId'
+}
+run_get_request_tests () {
+ mkdir -p "$PATH_TO_KMP_GET_REQUEST_TEST_RESULTS"
+ docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_get_request'
+}
+run_set_survey_tests () {
+ mkdir -p "$PATH_TO_KMP_SET_SURVEY_TEST_RESULTS"
+ docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_set_survey'
+}
+run_cache_instance_tests () {
+ mkdir -p "$PATH_TO_KMP_CACHE_INSTANCE_TEST_RESULTS"
+ docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_cache_instance'
+}
+run_obtain_cached_instance_tests () {
+ mkdir -p "$PATH_TO_KMP_OBTAIN_CACHED_INSTANCE_TEST_RESULTS"
+ docker exec -it enketo sh -c 'cd /app && /app/node_modules/.bin/grunt test-and-build-kmp_obtain_cached_instance'
+}
+
 
 export path_to_test_file="/home/patrick/KMP/testAutomation/project/enketo/enketo-express-2/test/server/testCases/testCase001.spec.js"
 export path_to_test_dir="/home/patrick/KMP/testAutomation/project/enketo/enketo-express-2/test/server"
+
 
 
 # path_to_kmp="$(/home/patrick/KMP/testAutomation)"
@@ -40,73 +74,197 @@ export path_to_test_dir="/home/patrick/KMP/testAutomation/project/enketo/enketo-
 #done
 
 
-input_line_1=$(cat testCase001.txt | grep "Inputs : ")
-test001_input=${input_line_1##*:}
+input_line_1=$(cat testCase001.txt | grep -A 10 "it(")
 
-input_line_2=$(cat testCase002.txt | grep "Inputs : ")
-test002_input=${input_line_2##*:}
+input_line_2=$(cat testCase002.txt | grep -A 10 "it(")
 
-input_line_3=$(cat testCase003.txt | grep "Inputs : ")
-test003_input=${input_line_3##*:}
+input_line_3=$(cat testCase003.txt | grep -A 10 "it(")
 
-input_line_4=$(cat testCase004.txt | grep "Inputs : ")
-test004_input=${input_line_4##*:}
+input_line_4=$(cat testCase004.txt | grep -A 10 "it(")
 
-input_line_5=$(cat testCase005.txt | grep "Inputs : ")
-test005_input=${input_line_5##*:}
+input_line_5=$(cat testCase005.txt | grep -A 10 "it(")
 
-input_line_6=$(cat testCase006.txt | grep "Inputs : ")
-test006_input=${input_line_6##*:}
+input_line_6=$(cat testCase006.txt | grep -A 10 "it(")
 
-input_line_7=$(cat testCase007.txt | grep "Inputs : ")
-test007_input=${input_line_7##*:}
+input_line_7=$(cat testCase007.txt | grep -A 10 "it(")
 
-input_line_8=$(cat testCase008.txt | grep "Inputs : ")
-test008_input=${input_line_8##*:}
+input_line_8=$(cat testCase008.txt | grep -A 10 "it(")
 
-input_line_9=$(cat testCase009.txt | grep "Inputs : ")
+input_line_9=$(cat testCase009.txt | grep -A 10 "it(")
 test009_input=${input_line_9##*:}
 
-input_line_10=$(cat testCase010.txt | grep "Inputs : ")
-test010_input=${input_line_10##*:}
+input_line_11=$(cat testCase011.txt | grep -A 10 "it(")
+test011_input=${input_line_11##*:}
 
-input_line_11=$(cat testCase011.txt | grep "Inputs : ")
-input_line_12=$(cat testCase012.txt | grep "Inputs : ")
-input_line_13=$(cat testCase013.txt | grep "Inputs : ")
-input_line_14=$(cat testCase014.txt | grep "Inputs : ")
-input_line_15=$(cat testCase015.txt | grep "Inputs : ")
-input_line_16=$(cat testCase016.txt | grep "Inputs : ")
-input_line_17=$(cat testCase017.txt | grep "Inputs : ")
-input_line_18=$(cat testCase018.txt | grep "Inputs : ")
-input_line_19=$(cat testCase019.txt | grep "Inputs : ")
-input_line_20=$(cat testCase020.txt | grep "Inputs : ")
-input_line_21=$(cat testCase021.txt | grep "Inputs : ")
-input_line_22=$(cat testCase022.txt | grep "Inputs : ")
-input_line_23=$(cat testCase023.txt | grep "Inputs : ")
-input_line_24=$(cat testCase024.txt | grep "Inputs : ")
-input_line_25=$(cat testCase025.txt | grep "Inputs : ")
+input_line_12=$(cat testCase012.txt | grep -A 10 "it(")
+test012_input=${input_line_12##*:}
+
+input_line_13=$(cat testCase013.txt | grep -A 10 "it(")
+test013_input=${input_line_13##*:}
+
+input_line_14=$(cat testCase014.txt | grep -A 10 "it(")
+test014_input=${input_line_14##*:}
+
+input_line_15=$(cat testCase015.txt | grep -A 10 "it(")
+test015_input=${input_line_15##*:}
+
+input_line_16=$(cat testCase016.txt | grep -A 10 "it(")
+test016_input=${input_line_16##*:}
+
+input_line_17=$(cat testCase017.txt | grep -A 10 "it(")
+test017_input=${input_line_17##*:}
+
+input_line_18=$(cat testCase018.txt | grep -A 10 "it(")
+test018_input=${input_line_18##*:}
+
+input_line_19=$(cat testCase019.txt | grep -A 10 "it(")
+test019_input=${input_line_19##*:}
+
+input_line_20=$(cat testCase020.txt | grep -A 10 "it(")
+test011_input=${input_line_11##*:}
+
+input_line_21=$(cat testCase021.txt | grep -A 10 "it(")
+test021_input=${input_line_21##*:}
+
+input_line_22=$(cat testCase022.txt | grep -A 10 "it(")
+test022_input=${input_line_22##*:}
+
+input_line_23=$(cat testCase023.txt | grep -A 10 "it(")
+test023_input=${input_line_23##*:}
+
+input_line_24=$(cat testCase024.txt | grep -A 10 "it(")
+test024_input=${input_line_24##*:}
+
+input_line_25=$(cat testCase025.txt | grep -A 10 "it(")
+
+input_line_26=$(cat testCase026.txt | grep -A 10 "it(")
+
+input_line_27=$(cat testCase027.txt | grep -A 10 "it(")
 
 
-id_test_inputs=("$test001_input" "$test002_input" "$test003_input")
 
-instanceId_test_inputs=("$test006_input" "$test007_input" "$test008_input" "$test009_input" "$test010_input")
+id_test_inputs=("$input_line_1" "$input_line_2" "$input_line_3" "$input_line_4" "$input_line_5")
+
+instanceId_test_inputs=("$input_line_6" "$input_line_7" "$input_line_8" "$input_line_9")
+
+get_request_test_inputs=("$input_line_11" "$input_line_12" "$input_line_13")
+
+set_survey_test_inputs=("$input_line_14" "$input_line_15" "$input_line_16" "$input_line_17" "$input_line_18")
+
+cache_instance_test_inputs=("$input_line_19" "$input_line_20" "$input_line_21" "$input_line_22" "$input_line_23" "$input_line_24")
+
+obtain_cached_instance_test_inputs=("$input_line_25" "$input_line_26" "$input_line_27")
 
 
 get_new_id_path() {
-  path_to_test_file="$path_to_test_dir/id-$1.spec.js"
+  path_to_test_file="$path_to_test_dir/id-$COUNTER.spec.js"
 }
 
 get_new_instanceId_path() {
-  path_to_test_file="$path_to_dir/instanceId_tests/id_$1.spec.js"
+  path_to_test_file="$path_to_test_dir/instanceId-$COUNTER.spec.js"
 }
-for i in ${id_test_inputs[@]}
+
+get_new_get_request_path() {
+  path_to_test_file="$path_to_test_dir/get_request-$COUNTER.spec.js"
+}
+
+get_new_survey_test_path() {
+  path_to_test_file="$path_to_test_dir/set_survey-$COUNTER.spec.js"
+}
+
+get_new_cache_instance_path() {
+  path_to_test_file="$path_to_test_dir/cache_instance-$COUNTER.spec.js"
+}
+
+get_new_obtain_cached_instance_path() {
+  path_to_test_file="$path_to_test_dir/obtain_cached_instance-$COUNTER.spec.js"
+}
+
+
+COUNTER=0
+for i in "${id_test_inputs[@]}"
 do
  get_new_id_path "$i"
- echo "$i"
  echo "$path_to_test_file"
  generate_id_test "$i"
+ COUNTER=$((COUNTER+1))
 done
-TEST_RESULTS="$(run_id_tests)"
-echo "$TEST_RESULTS" > "$PATH_TO_KMP_ID_TEST_RESULTS/test-and-build-kmp_id-$TIME_EXECUTED.html"
+ID_TEST_RESULTS="$(run_id_tests)"
+echo "$ID_TEST_RESULTS" > "$PATH_TO_KMP_ID_TEST_RESULTS/test-and-build-kmp_id-$TIME_EXECUTED.html"
+echo "$ID_TEST_RESULTS" >> "$TIME_EXECUTED.total.html"
+for row in $(echo "${ID_TEST_RESULTS}" | jq -r '.passes[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+   echo ${row} | base64 --decode >> /home/patrick/KMP/testCases/newJSfile.js
+   echo  $(_jq '.') >> /home/patrick/KMP/testCases/newJSfile.js
+done
+
+for i in "${instanceId_test_inputs[@]}"
+do
+ get_new_instanceId_path "$i"
+ echo "$path_to_test_file"
+ generate_instanceId_test "$i"
+ COUNTER=$((COUNTER+1))
+done
+INSTANCEID_TEST_RESULTS="$(run_instanceId_tests)"
+echo "$INSTANCEID_TEST_RESULTS" > "$PATH_TO_KMP_INSTANCEID_TEST_RESULTS/test-and-build-kmp_instanceId-$TIME_EXECUTED.html"
+echo "$INSTANCEID_TEST_RESULTS" >> "$TIME_EXECUTED.total.html"
+
+
+#for i in ${set_survey_test_inputs[@]}
+for i in "${set_survey_test_inputs[@]}"
+do
+ get_new_survey_test_path "$i"
+ echo "$path_to_test_file"
+ generate_set_survey_test "$i"
+ COUNTER=$((COUNTER+1))
+done
+SET_SURVEY_TEST_RESULTS="$(run_set_survey_tests)"
+echo "$SET_SURVEY_TEST_RESULTS" > "$PATH_TO_KMP_SET_SURVEY_TEST_RESULTS/test-and-build-kmp_set_survey-$TIME_EXECUTED.html"
+
+for i in "${cache_instance_test_inputs[@]}"
+do
+ get_new_cache_instance_path "$i"
+ echo "$path_to_test_file"
+ generate_cache_instance_test "$i"
+ COUNTER=$((COUNTER+1))
+done
+CACHE_INSTANCE_TEST_RESULTS="$(run_cache_instance_tests)"
+echo "$CACHE_INSTANCE_TEST_RESULTS" > "$PATH_TO_KMP_CACHE_INSTANCE_TEST_RESULTS/test-and-build-kmp_cache_instance-$TIME_EXECUTED.html"
+
+for i in "${obtain_cached_instance_test_inputs[@]}"
+do
+ get_new_obtain_cached_instance_path "$i"
+ echo "$path_to_test_file"
+ generate_obtain_cached_instance_test "$i"
+ COUNTER=$((COUNTER+1))
+done
+OBTAIN_CACHED_INSTANCE_TEST_RESULTS="$(run_obtain_cached_instance_tests)"
+echo "$OBTAIN_CACHED_INSTANCE_TEST_RESULTS" > "$PATH_TO_KMP_OBTAIN_CACHED_INSTANCE_TEST_RESULTS/test-and-build-kmp_obtain_cached_instance-$TIME_EXECUTED.html"
+
+concat_and_open_all_test_results () {
+ mkdir -p "$PATH_TO_ALL_TEST_RESULTS"
+ echo "$ID_TEST_RESULTS" >> "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+ echo "$INSTANCEID_TEST_RESULTS" >> "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+ echo "$SET_SURVEY_TEST_RESULTS" >> "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+ echo "$CACHE_INSTANCE_TEST_RESULTS" >> "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+ echo "$OBTAIN_CACHED_INSTANCE_TEST_RESULTS" >> "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+}
+
+concat_and_open_all_test_results
+# xdg-open "$PATH_TO_ALL_TEST_RESULTS/$TIME_EXECUTED.total.html"
+ 
+#for i in ${get_request_test_inputs[@]}
+#do
+# counter=7
+# get_new_get_request_path "$i"
+# echo "test number : "$counter" "$i""
+# echo "$path_to_test_file"
+# generate_get_request_test "$i"
+# counter=$((counter+1))
+#done
+#GET_REQUEST_TEST_RESULTS="$(run_get_request_tests)"
+#echo "$GET_REQUEST_TEST_RESULTS" > "$PATH_TO_KMP_GET_REQUEST_TEST_RESULTS/test-and-build-kmp_get_request-$TIME_EXECUTED.html"
 # echo "3 $test001_input"
 # echo "4 $id_test_input"
--- testAutomation/testCases/something.sh
+++ testAutomation/testCases/something.sh
@@ -45,11 +45,321 @@ describe( 'Survey Model', function() {
     var id = 'AAAA';
     var instanceId = 'uuid:BBBB';
 
-    it( 'is rejected if id is undefined', function() {
-            var test = submission.isNew( $variable, instanceId );
-            return expect( test ).to.eventually.be.rejected.and.have.property( 'status' ).that.equals( 400 );
-        } );
+    $1
+  })
+})
+EOF
+}
+generate_instanceId_test() {
+  variable="$1"
+  cat > $path_to_test_file <<EOF
+/* global describe, require, it, afterEach */
+'use strict';
+
+// safer to ensure this here (in addition to grunt:env:test)
+process.env.NODE_ENV = 'test';
+
+var chai = require( 'chai' );
+var expect = chai.expect;
+var chaiAsPromised = require( 'chai-as-promised' );
+var redis = require( 'redis' );
+var config = require( '../../app/models/config-model' ).server;
+config[ 'base path' ] = '';
+var submission = require( '../../app/models/submission-model' );
+var client = redis.createClient( config.redis.main.port, config.redis.main.host, {
+    auth_pass: config.redis.main.password
+} );
+
+chai.use( chaiAsPromised );
+
+describe( 'Survey Model', function() {
+
+  afterEach( function( done ) {
+    // select test database and flush it
+    client.select( 15, function( err ) {
+      if ( err ) {
+        return done( err );
+      }
+      client.flushdb( function( err ) {
+        if ( err ) {
+          return done( err );
+        }
+        done();
+      });
+    });
+  });
+
+  describe( 'isNew() check', function() {
+    var id = 'AAAA';
+    var instanceId = 'uuid:BBBB';
+
+    $1
+  })
+})
+EOF
+}
+generate_get_request_test() {
+  variable="$1"
+  cat > $path_to_test_file <<EOF
+  /* global describe, require, it, beforeEach, afterEach */
+  'use strict';
+
+  /*
+   * These tests use the special test Api Token and Server URLs defined in the API spec
+   * at http://apidocs.enketo.org.
+   */
+
+  // safer to ensure this here (in addition to grunt:env:test)
+  process.env.NODE_ENV = 'test';
+
+  var request = require( 'supertest' );
+  var app = require( '../../config/express' );
+  var surveyModel = require( '../../app/models/survey-model' );
+  var instanceModel = require( '../../app/models/instance-model' );
+  var redis = require( 'redis' );
+  var config = require( '../../app/models/config-model' ).server;
+  var client = redis.createClient( config.redis.main.port, config.redis.main.host, {
+      auth_pass: config.redis.main.password
+  } );
+
+  describe( 'Submissions', function() {
+      var enketoId;
+      var nonExistingEnketoId = 'nope';
+      var validServer = 'https://testserver.com/bob';
+      var validFormId = 'something';
+
+      beforeEach( function( done ) {
+          // add survey if it doesn't exist in the db
+          surveyModel.set( {
+              openRosaServer: validServer,
+              openRosaId: validFormId,
+          } ).then( function( id ) {
+              enketoId = id;
+              done();
+          } );
+      } );
+
+      describe( 'using GET (existing submissions) for an existing/active Enketo IDs', function() {
+
+          $1
+
+          describe( 'for a valid and existing instanceID that does not belong to the current form', function() {
+
+              beforeEach( function( done ) {
+                  // add survey if it doesn't exist in the db
+                  instanceModel.set( {
+                      openRosaServer: validServer,
+                      openRosaId: 'differentId',
+                      instanceId: 'b',
+                      returnUrl: 'example.com',
+                      instance: '<data></data>',
+                      instanceAttachments: {
+                          'test.jpg': 'https://example.com'
+                      }
+                  } ).then( function() {
+                      done();
+                  } );
+              } );
+
+              it( 'responds with 400', function( done ) {
+                  request( app ).get( '/submission/::' + enketoId + '?instanceId=b' )
+                      .expect( 400, done );
+              } );
+
+          } );
+
+          describe( 'for a valid and existing instanceID that belongs to the current form', function() {
+
+              beforeEach( function( done ) {
+                  // add survey if it doesn't exist in the db
+                  instanceModel.set( {
+                      openRosaServer: validServer,
+                      openRosaId: validFormId,
+                      instanceId: 'c',
+                      returnUrl: 'example.com',
+                      instance: '<data></data>'
+                  } ).then( function() {
+                      done();
+                  } );
+              } );
+
+              it( 'responds with 200', function( done ) {
+                  request( app ).get( '/submission/::' + enketoId + '?instanceId=c' )
+                      .expect( 200, done );
+              } );
+
+          } );
+
+      } );
+
+  } );
+EOF
+}
+generate_set_survey_test() {
+  variable="$1"
+  #echo "var : $variable"
+  cat > $path_to_test_file <<EOF
+  /* global describe, require, it, beforeEach, afterEach */
+  'use strict';
+
+  // safer to ensure this here (in addition to grunt:env:test)
+  process.env.NODE_ENV = 'test';
+
+  var _wait1ms;
+  var Promise = require( 'lie' );
+  var chai = require( 'chai' );
+  var expect = chai.expect;
+  var chaiAsPromised = require( 'chai-as-promised' );
+  var redis = require( 'redis' );
+  var config = require( '../../app/models/config-model' ).server;
+  var model = require( '../../app/models/survey-model' );
+  var client = redis.createClient( config.redis.main.port, config.redis.main.host, {
+      auth_pass: config.redis.main.password
+  } );
+
+  chai.use( chaiAsPromised );
+
+  // help function to ensure subsequent database entries don't have the exact same timestamp
+  // redis is fast...
+  _wait1ms = function() {
+      return new Promise( function( resolve ) {
+          setTimeout( function() {
+              resolve();
+          }, 1 );
+      } );
+  };
+
+  describe( 'Survey Model', function() {
+
+      afterEach( function( done ) {
+          // select test database and flush it
+          client.select( 15, function( err ) {
+              if ( err ) {
+                  return done( err );
+              }
+              client.flushdb( function( err ) {
+                  if ( err ) {
+                      return done( err );
+                  }
+                  done();
+              } );
+          } );
+      } );
+
+      describe( 'set: when attempting to store new surveys', function() {
+          var survey;
+
+          beforeEach( function() {
+              survey = {
+                  openRosaId: 'widgets',
+                  openRosaServer: 'https://ona.io/enketo'
+              };
+          } );
+
+          $1
   } );
 } );
+EOF
+}
+generate_cache_instance_test() {
+  variable="$1"
+  #echo "var : $variable"
+  cat > $path_to_test_file <<EOF
+  /* global describe, require, it, beforeEach, afterEach */
+  'use strict';
+
+  var Promise = require( 'lie' );
+  var chai = require( 'chai' );
+  var expect = chai.expect;
+  var chaiAsPromised = require( 'chai-as-promised' );
+  var model = require( '../../app/models/instance-model' );
+
+  chai.use( chaiAsPromised );
+
+  describe( 'Instance Model', function() {
+
+      afterEach( function( done ) {
+          model.remove( {
+                  instanceId: 'someid'
+              } )
+              .then( function() {
+                  done();
+              } )
+              .catch( function() {
+                  done();
+              } );
+      } );
+
+      describe( 'set: when attempting to cache an instance', function() {
+          var survey;
+
+          beforeEach( function() {
+              survey = {
+                  openRosaId: 'widgets',
+                  openRosaServer: 'http://example.com',
+                  instanceId: 'someid',
+                  returnUrl: 'http://example.com',
+                  instance: '<data></data>',
+                  instanceAttachments: {
+                      'test.jpg': 'https://example.com/test.jpg'
+                  }
+              };
+          } );
+
+          $1
+          } );
+
+
+  } );
+EOF
+}
+generate_obtain_cached_instance_test() {
+  variable="$1"
+  cat > $path_to_test_file <<EOF
+  /* global describe, require, it, beforeEach, afterEach */
+  'use strict';
+
+  var Promise = require( 'lie' );
+  var chai = require( 'chai' );
+  var expect = chai.expect;
+  var chaiAsPromised = require( 'chai-as-promised' );
+  var model = require( '../../app/models/instance-model' );
+
+  chai.use( chaiAsPromised );
+
+  describe( 'Instance Model', function() {
+
+      afterEach( function( done ) {
+          model.remove( {
+                  instanceId: 'someid'
+              } )
+              .then( function() {
+                  done();
+              } )
+              .catch( function() {
+                  done();
+              } );
+      } );
+      describe( 'get: when attempting to obtain a cached instance', function() {
+          var survey;
+          var promise;
+
+          beforeEach( function() {
+              survey = {
+                  openRosaId: 'widgets',
+                  openRosaServer: 'http://example.com',
+                  instanceId: 'someid',
+                  returnUrl: 'http://example.com',
+                  instance: '<data></data>',
+                  instanceAttachments: {
+                      'test.jpg': 'https://example.com/test.jpg'
+                  }
+              };
+          } );
+
+          $1
+
+          } );
+    } );
 EOF
 }
